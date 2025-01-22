14afadf00b4f99d1e2b5a4ec4c38cc140be3f3b1
William McDermott <williammcdermott@MacBookPro.lan>
2025-01-22 15:56:23 -0600

    Use interceptor to remove password from getUser respoinse

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -12,9 +13,10 @@ export class UsersController {
         this.usersService.create(body.email, body.password)
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
+        console.log('handler is running');
         const user = await this.usersService.findOne(parseInt(id));
         if (!user) {
             throw new NotFoundException('user not found');
