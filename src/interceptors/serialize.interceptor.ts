85f8ac8345af6e7367ae3e92a4b519555e891810
William McDermott <williammcdermott@MacBookPro.lan>
2025-01-22 16:07:40 -0600

    Wrap interceptor in a decorator

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,11 +1,16 @@
 import {
     NestInterceptor,
     ExecutionContext,
-    CallHandler
+    CallHandler,
+    UseInterceptors
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+
+export function Serialize(dto: any) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 
 export class SerializeInterceptor implements NestInterceptor {
     constructor(private dto: any) {}
