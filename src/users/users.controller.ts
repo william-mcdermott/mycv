87be8bdcbaee92ce9290b439b5a6c50a649ad736
William McDermott <williammcdermott@MacBookPro.lan>
2025-01-23 00:28:42 -0600

    Implement currentuser decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,15 +5,21 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService, private authService: AuthService) {}
 
+    // @Get('/whoami')
+    // whoAmI(@Session() session: any) {
+    //     return this.usersService.findOne(session.userId);
+    // }
+
     @Get('/whoami')
-    whoAmI(@Session() session: any) {
-        return this.usersService.findOne(session.userId);
+    whoAmI(@CurrentUser() user: string) {
+        return user;
     }
 
     @Post('/signout')
