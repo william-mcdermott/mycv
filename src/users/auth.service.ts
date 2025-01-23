c8684c2f7ff87ca9d568a5eb6537b67e26907064
William McDermott <williammcdermott@MacBookPro.lan>
2025-01-22 21:58:24 -0600

    Create user with hashed password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -17,6 +17,8 @@ export class AuthService {
         const salt = randomBytes(8).toString('hex');
         const hash = (await scrypt(password, salt, 32)) as Buffer;
         const result = salt + '.' + hash.toString('hex');
+        const user = await this.usersService.create(email, result);
+        return user;
     }
 
     signin() {
