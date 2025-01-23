cb408f7e226af7f4e213e54fe14af0a2ea8c0e23
William McDermott <williammcdermott@MacBookPro.lan>
2025-01-22 18:13:32 -0600

    implement signup method in AuthService

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService) {}
+
+    async signup(email: string, password: string) {
+        const users = await this.usersService.find(email);
+        if (users.length) {
+            throw new BadRequestException('email in use');
+        }
+    }
+
+    signin() {
+
+    }
 }
