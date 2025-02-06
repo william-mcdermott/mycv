bc40a8eb523739329ad26a8302f56fbe848156aa
William McDermott <williammcdermott@MacBookPro.lan>
2025-02-06 10:30:59 -0600

    Authorize admin user to approve reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -7,6 +7,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -20,6 +21,7 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
         return this.reportsService.changeApproval(id, body.approved)
     }
