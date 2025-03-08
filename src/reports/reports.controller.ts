dd9f4b8daf475596ebbd3d075372c8404d571a6f
William McDermott <williammcdermott@MacBookPro.lan>
2025-03-08 13:35:53 -0600

    set e2e tests to run migrations before testing

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post, UseGuards, Patch, Param, Get, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
-import { User } from 'src/users/user.entity';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
-import { AdminGuard } from 'src/guards/admin.guard';
+import { AdminGuard } from '../guards/admin.guard';
 import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Report } from "src/reports/report.entity";
+import { Report } from "../reports/report.entity";
 import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 
 @Entity()
