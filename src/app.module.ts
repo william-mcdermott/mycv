fae24c99da142b935c2660eaf740923c8924e028
William McDermott <williammcdermott@MacBookPro.lan>
2025-03-07 19:59:29 -0600

    Make ormconfig variable based on environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+const dbConfig = require('../ormconfig.js');
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -16,17 +15,7 @@ const cookieSession = require('cookie-session');
         isGlobal: true,
         envFilePath: `.env.${process.env.NODE_ENV}`
     }),
-    TypeOrmModule.forRootAsync({
-        inject: [ConfigService],
-        useFactory: (config: ConfigService) => {
-            return {
-                type: 'sqlite',
-                database: config.get<string>('DB_NAME'),
-                synchronize: true,
-                entities: [User, Report]
-            }
-        }
-    }),
+    TypeOrmModule.forRoot(dbConfig),
     UsersModule,
     ReportsModule
   ],
